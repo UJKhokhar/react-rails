1d8272f65ae7f37820487d6bf55bc3fe2be5dee4
ujkhokhar <ujkhokhar@gmail.com>
2015-04-21 22:50:11 -0400

    Remove authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :authenticate
   
   protected
 
   def configure_permitted_parameters  
     devise_parameter_sanitizer.for(:sign_up) << :name
-  end
-
-  def authenticate
-    authenticate_token || render_unauthorized
-  end
-
-  def authenticate_token
-    authenticate_with_http_token do |token, option|
-      User.find_by(auth_token: token)
-    end
-  end
-
-  def render_unauthorized
-    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
-    render json: 'Bad credentials', status: 401
   end
 end
